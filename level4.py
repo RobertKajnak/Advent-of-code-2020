cbe9c3060e26377eb7545aa0d008b3203487a4b2
RobertKajnak <hezirisz@gmail.com>
2020-12-05 10:54:21 +0900

    Added catch to be more theoretically correct

--- level4.py
+++ level4.py
@@ -21,43 +21,46 @@ with open('input-4-1.txt', 'r') as f:
                 if r not in pp:
                     break
             else:
-                val = int(pp['byr'])
-                if val<1920 or val>2002:
-                    print(val)
+                try:
+                    val = int(pp['byr'])
+                    if val<1920 or val>2002:
+                        print(val)
+                        continue
+                    
+                    val = int(pp['iyr'])
+                    if val<2010 or val>2020:
+                        print(val)
+                        continue
+                    
+                    val = int(pp['eyr'])
+                    if val<2020 or val>2030:
+                        print(val)
+                        continue
+                    
+                    val = pp['hgt']
+                    if not ((val[-2:]=='cm' and int(val[:-2])>=150 and int(val[:-2])<=193) or\
+                        (val[-2:]=='in' and int(val[:-2])>=59 and int(val[:-2])<=76)):
+                        print(val)
+                        continue
+                    
+                    val = pp['hcl']
+                    if not (val[0]=='#' and len(val)==7 and int(val[1:],16)):
+                        print(val)
+                        continue
+                    
+                    val = pp['ecl']
+                    if val not in ['amb', 'blu', 'brn', 'gry', 'grn', 'hzl', 'oth']:
+                        print(val)
+                        continue
+                    
+                    val = pp['pid']
+                    if len(val)!=9:
+                        print(val)
+                        continue
+                    int(val)
+                except Exception as e:
+                    print(e)
                     continue
-                
-                val = int(pp['iyr'])
-                if val<2010 or val>2020:
-                    print(val)
-                    continue
-                
-                val = int(pp['eyr'])
-                if val<2020 or val>2030:
-                    print(val)
-                    continue
-                
-                val = pp['hgt']
-                if not ((val[-2:]=='cm' and int(val[:-2])>=150 and int(val[:-2])<=193) or\
-                    (val[-2:]=='in' and int(val[:-2])>=59 and int(val[:-2])<=76)):
-                    print(val)
-                    continue
-                
-                val = pp['hcl']
-                if not (val[0]=='#' and len(val)==7 and int(val[1:],16)):
-                    print(val)
-                    continue
-                
-                val = pp['ecl']
-                if val not in ['amb', 'blu', 'brn', 'gry', 'grn', 'hzl', 'oth']:
-                    print(val)
-                    continue
-                
-                val = pp['pid']
-                if len(val)!=9:
-                    print(val)
-                    continue
-                int(val)
-                
                 print('Valid:', pp)
                 S+=1
             L= ''
